17d98359113828c034b60d2daa86c5be7cb52d60
Tasneem <bhatitasneem@gmail.com>
2022-07-16 14:37:10 +0530

    First Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam et corrupti recusandae consequuntur dolorem placeat laborum cum consequatur numquam reiciendis velit sequi sit eaque, est doloremque iusto unde laudantium deleniti nesciunt quaerat autem minus.
+            </p>
+        </div>
+    )
+}
